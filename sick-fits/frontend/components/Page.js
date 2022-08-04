7db77004b67ee9926152ee64d0c73c4674eddd9d
Jimel Joseph <3507684+jimelj@users.noreply.github.com>
2022-08-04 10:03:26 -0400

    working with graphql

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -16,11 +16,12 @@ const GlobalStyles = createGlobalStyle`
     --grey: #3a3a3a;
     --gray: var(--grey);
     --lightGrey: #e1e1e1;
-    --lightGray: (--lightGrey);
+    --lightGray: var(--lightGrey);
     --offWhite: #ededed;
     --maxWidth: 1000px;
     --bs: 0 12px 24px rgba(0,0,0,0.09);
     box-sizing: border-box;
+    font-size: 62.5%;
   }
   *, *:before, *:after {
     box-sizing: inherit;
